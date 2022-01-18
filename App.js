38c4a7c336560e0fa41f276f8bf0fa61699c92ae
prare11 <97444293+prare11@users.noreply.github.com>
2022-01-18 22:29:12 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <>
+   <h1 class="Wooder">W O O D E R</h1>
+   <Header/>  
+    </>
+  );
+}
+
+export default App;
